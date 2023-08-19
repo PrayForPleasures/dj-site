423e98b9defdbfc86a4c68183f886e00cd36e298
PrayForPleasures <dmitriyyarchucK@gmail.com>
2023-08-20 01:27:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Header from "./components/header/Header";
 import Line from "./components/line/Line";
 import About from "./components/about/About";
+import backImage from "./images/backimg.jpg";
 
 function App() {
 	return (
-		<div className="container">
-			<Line></Line>
-			<Header></Header>
-			<Line></Line>
-			<About></About>
+		<div className="back_image">
+			<div className="container">
+				<Line></Line>
+				<Header></Header>
+				<Line></Line>
+				<About></About>
+			</div>
 		</div>
 	);
 }
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,29 +1,12 @@
 import styles from "../about/About.module.css";
-import dj1 from "../../images/dj1.jpg";
-import dj2 from "../../images/dj2.jpg";
-import dj3 from "../../images/dj3.jpg";
-import dj4 from "../../images/dj4.jpg";
-import djm from "../../images/djm.jpg";
 
 function About() {
 	return (
 		<div className={styles.about}>
-			<div className={styles.grid__parent}>
-				<div className={styles.grid__item1}>
-					<img className={styles.grid__image} src={dj1} alt="image" />
-				</div>
-				<div className={styles.grid__item2}>
-					<img className={styles.grid__image} src={dj2} alt="image" />
-				</div>
-				<div className={styles.grid__item3}>
-					<img className={styles.grid__image} src={dj3} alt="image" />
-				</div>
-				<div className={styles.grid__item4}>
-					<img className={styles.grid__image} src={dj4} alt="image" />
-				</div>
-				<div className={styles.grid__item_main}>
-					<img className={styles.grid__image_main} src={djm} alt="image" />
-				</div>
+			<div className={styles.about__boxex}>
+				<div className={styles.box1}></div>
+				<div className={styles.box2}></div>
+				<div className={styles.box3}></div>
 			</div>
 		</div>
 	);
